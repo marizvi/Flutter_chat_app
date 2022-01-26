23ee60d8f619ea1b7151a6bd2ff15aefab8b593f
marizvi <202051121@iiitvadodara.ac.in>
2022-01-26 17:30:10 +0530

    auth screen added

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,7 +1,6 @@
 import 'package:chat_app/widget/auth_form.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart';
 
 class AuthScreeen extends StatefulWidget {
   @override
