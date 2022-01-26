c429090a7388fd4e4536e3119c02434489d303e6
marizvi <202051121@iiitvadodara.ac.in>
2022-01-27 00:04:52 +0530

    messages added

---
+++ lib/widget/chat/new_message.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
