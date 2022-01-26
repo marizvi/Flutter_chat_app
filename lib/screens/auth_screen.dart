860b3f20dcb9bf7e766b51c92a908543bf79ea8f
marizvi <202051121@iiitvadodara.ac.in>
2022-01-26 17:29:05 +0530

    auth screen added

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,7 @@
 import 'package:chat_app/widget/auth_form.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 class AuthScreeen extends StatefulWidget {
   @override
@@ -7,11 +9,47 @@ class AuthScreeen extends StatefulWidget {
 }
 
 class _AuthScreeenState extends State<AuthScreeen> {
+  //earlier firebase authentication was through rest api
+  //here we are actually going to use firebase authentication sdk
+
+  final _auth = FirebaseAuth.instance;
+  void _submitAuthForm(
+    String? email,
+    String? password,
+    String? username,
+    bool isLogin,
+    BuildContext ctx,
+  ) async {
+    UserCredential authResult;
+    try {
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email as String, password: password as String);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email as String, password: password as String);
+      }
+    } on FirebaseAuthException catch (err) {
+      // errors returned by password regarding invalid email or password
+      print(err.message);
+      var message = 'An error Occured please check your credentials!';
+      if (err.message != null) {
+        message = err.message as String;
+      }
+      //since for snackbar we need context so we took it through constructor
+      Scaffold.of(ctx).showSnackBar(SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(ctx).errorColor,
+      ));
+    } catch (err) {
+      print(err);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: AuthForm(),
-    );
+        backgroundColor: Theme.of(context).primaryColor,
+        body: AuthForm(_submitAuthForm));
   }
 }
--- lib/widget/auth_form.dart
+++ lib/widget/auth_form.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
+  final Function(String? email, String? password, String? username,
+      bool isLogin, BuildContext ctx) submitForm;
+  AuthForm(this.submitForm);
   @override
   _AuthFormState createState() => _AuthFormState();
 }
 
-class _AuthFormState extends State<AuthForm> {
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
   final _formKey = GlobalKey<FormState>();
   final _passwordController = TextEditingController();
   var _isLogin = true;
   String? _userEmail = '';
   String? _userName = '';
   String? _userPassword = '';
+  AnimationController? controller;
+  Animation<double>? _opacityAnimation;
+  Animation<Offset>? _slideAnimation;
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 280));
+    _slideAnimation = Tween(begin: Offset(0, -0.7), end: Offset(0, 0)).animate(
+        //Offset(x,y)
+        CurvedAnimation(
+            parent: controller as Animation<double>,
+            curve: Curves.easeInOutQuad));
+
+    _opacityAnimation = Tween(begin: 0.5, end: 1.0).animate(CurvedAnimation(
+        parent: controller as Animation<double>, curve: Curves.easeInSine));
+  }
 
   void _trySubmit() {
     FocusScope.of(context).unfocus(); //this will close the keyboard
@@ -21,112 +42,134 @@ class _AuthFormState extends State<AuthForm> {
     if (isValid) {
       // .save() will trigger onSaved: for all textFormField
       _formKey.currentState!.save();
-      print(_userEmail);
-      print(_userName);
-      print(_userPassword);
+      //trim() is use to remove leading and trailing white spaces
+      widget.submitForm(
+        _userEmail!.trim(),
+        _userPassword!.trim(),
+        _userName!.trim(),
+        _isLogin,
+        context,
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    if (!_isLogin) {
+      controller!.forward();
+    } else {
+      controller!.reverse();
+    }
+
     return Container(
       decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage('assets/images/bb6.jpg'), fit: BoxFit.fill)),
       child: Center(
-        child: Card(
-          margin: EdgeInsets.all(12),
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-          elevation: 15,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.all(12),
-              child: Form(
-                key: _formKey,
-                child: Column(mainAxisSize: MainAxisSize.min, children: [
-                  TextFormField(
-                      // key is necessary when we are changing number of
-                      // textform fields dynamically, to prvent swapping of values
-                      // among these fields
-                      key: ValueKey('userEmail'),
-                      validator: (value) {
-                        if (value!.isEmpty || !value.contains('@')) {
-                          return 'Please enter a valid email address';
-                        }
-                        return null; //means everything is alright
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(labelText: 'Email Address'),
-                      onSaved: (value) {
-                        _userEmail = value;
-                      }),
-                  if (!_isLogin)
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 260),
+          curve: Curves.easeInOutQuad,
+          height: _isLogin ? 270 : 390,
+          child: Card(
+            margin: EdgeInsets.all(12),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            elevation: 15,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: Form(
+                  key: _formKey,
+                  child: Column(mainAxisSize: MainAxisSize.min, children: [
                     TextFormField(
                         // key is necessary when we are changing number of
                         // textform fields dynamically, to prvent swapping of values
                         // among these fields
-                        key: ValueKey('userName'),
+                        key: ValueKey('userEmail'),
                         validator: (value) {
-                          if (value!.isEmpty || value.length < 4) {
-                            return 'Please enter at least 4 characters';
+                          if (value!.isEmpty || !value.contains('@')) {
+                            return 'Please enter a valid email address';
+                          }
+                          return null; //means everything is alright
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(labelText: 'Email Address'),
+                        onSaved: (value) {
+                          _userEmail = value;
+                        }),
+                    if (!_isLogin)
+                      FadeTransition(
+                        opacity: _opacityAnimation as Animation<double>,
+                        child: TextFormField(
+                            // key is necessary when we are changing number of
+                            // textform fields dynamically, to prvent swapping of values
+                            // among these fields
+                            key: ValueKey('userName'),
+                            validator: (value) {
+                              if (value!.isEmpty || value.length < 4) {
+                                return 'Please enter at least 4 characters';
+                              }
+                              return null; //means everything is alright.
+                            },
+                            decoration: InputDecoration(labelText: 'Username'),
+                            onSaved: (value) {
+                              _userName = value;
+                            }),
+                      ),
+                    TextFormField(
+                        key: ValueKey('password'),
+                        validator: (value) {
+                          //according to firebase password shQud be at least of 7 characters
+                          if (value!.isEmpty || value.length < 8) {
+                            return 'Password must be atleast 8 characters';
                           }
                           return null; //means everything is alright.
                         },
-                        decoration: InputDecoration(labelText: 'Username'),
+                        decoration: InputDecoration(labelText: 'Password'),
+                        controller: _passwordController,
+                        obscureText: true, //hide input text entered by user
                         onSaved: (value) {
-                          _userName = value;
+                          _userPassword = value;
                         }),
-                  TextFormField(
-                      key: ValueKey('password'),
-                      validator: (value) {
-                        //according to firebase password shQud be at least of 7 characters
-                        if (value!.isEmpty || value.length < 8) {
-                          return 'Password must be atleast 8 characters';
-                        }
-                        return null; //means everything is alright.
-                      },
-                      decoration: InputDecoration(labelText: 'Password'),
-                      controller: _passwordController,
-                      obscureText: true, //hide input text entered by user
-                      onSaved: (value) {
-                        _userPassword = value;
-                      }),
-                  if (!_isLogin)
-                    TextFormField(
-                      key: ValueKey('conifrmPassword'),
-                      validator: (value) {
-                        if (value != _passwordController) {
-                          return "password does not matches";
-                        }
-                        return null;
-                      },
-                      decoration:
-                          InputDecoration(labelText: 'Confirm Password'),
+                    if (!_isLogin)
+                      FadeTransition(
+                          opacity: _opacityAnimation as Animation<double>,
+                          child: TextFormField(
+                            key: ValueKey('conifrmPassword'),
+                            validator: (value) {
+                              if (value != _passwordController.text) {
+                                return "password does not matches";
+                              }
+                              return null;
+                            },
+                            obscureText: true,
+                            decoration:
+                                InputDecoration(labelText: 'Confirm Password'),
+                          )),
+                    SizedBox(
+                      height: 12,
                     ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Container(
-                    width: 90,
-                    child: ElevatedButton(
-                      onPressed: _trySubmit,
-                      child: Text(_isLogin ? 'Login' : 'Register'),
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18))),
+                    Container(
+                      width: 90,
+                      child: ElevatedButton(
+                        onPressed: _trySubmit,
+                        child: Text(_isLogin ? 'Login' : 'Register'),
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18))),
+                      ),
                     ),
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        setState(() {
-                          _isLogin = !_isLogin;
-                        });
-                      },
-                      child: Text(_isLogin
-                          ? 'Create new Account'
-                          : 'Already have an Account'))
-                ]),
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _isLogin = !_isLogin;
+                          });
+                        },
+                        child: Text(_isLogin
+                            ? 'Create new Account'
+                            : 'Already have an Account'))
+                  ]),
+                ),
               ),
             ),
           ),
