7954350f83c14992c91f0ccdf5f4bc6d4fb83b9e
marizvi <202051121@iiitvadodara.ac.in>
2022-01-30 13:37:33 +0530

    push notification added

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,25 @@ import 'package:chat_app/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+
+  print(
+      "Handling a background message: ${message.notification!.title}\n ${message.notification!.body}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 //2
   await Firebase.initializeApp();
+
+  FirebaseMessaging.onBackgroundMessage(
+    _firebaseMessagingBackgroundHandler,
+  ); //necessary to receive notifications when app is running in background
+
   runApp(const MyApp());
 }
 
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,37 @@
 import 'dart:convert';
 import 'package:chat_app/widget/chat/messages.dart';
 import 'package:chat_app/widget/chat/new_message.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class Chatscreen extends StatelessWidget {
+class Chatscreen extends StatefulWidget {
+  @override
+  State<Chatscreen> createState() => _ChatscreenState();
+}
+
+class _ChatscreenState extends State<Chatscreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    //https://firebase.flutter.dev/docs/messaging/usage/
+    FirebaseMessaging.instance
+        .requestPermission(); //this line is necessary for foreground
+    //for background go to main.dart file
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      // print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print(
+            'Message contained a notification: ${message.notification!.title} and ${message.notification!.body}');
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
